d1c4cdbf69ccb2ebbeb842b03e32fd9b60e0da3c
Chad <chadjcampbell@gmail.com>
2022-05-03 18:15:49 -0400

    add most of the functions, and cover SOME corner cases

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-// let backspace = document.querySelector('#backspace');
-// let data7 = document.querySelector('#data7');
-// let data8 = document.querySelector('#data8');
-// let data9 = document.querySelector('#data9');
-// let divide = document.querySelector('#divide');
-// let data4 = document.querySelector('#data4');
-// let data5 = document.querySelector('#data5');
-// let data6 = document.querySelector('#data6');
-// let multiply = document.querySelector('#multiply');
-// let data1 = document.querySelector('#data1');
-// let data2 = document.querySelector('#data2');
-// let data3 = document.querySelector('#data3');
-// let minus = document.querySelector('#minus');
-// let point = document.querySelector('#point');
-// let data0 = document.querySelector('#data0');
-
-
-
-function add(a,b) {
-    return a+b;
-};
-
-function subtract (a,b) {
-    return a-b;
-};
-
-function multiply (a,b) {
-    return a*b;
-};
-
-function divide (a,b) {
-    return a/b;
-};
 
 function operate(operator, a, b) {
-    if (operator=='+') {return add(a,b)};
-    if (operator=='-') {return subtract(a,b)};
-    if (operator=='*') {return multiply(a,b)};
-    if (operator=='/') {return divide(a,b)};
+    if (operator=='+') {return (a+b)};
+    if (operator=='-') {return (a-b)};
+    if (operator=='*') {return (a*b)};
+    if (operator=='/') {return (a/b)};
 };
 
 let display = document.querySelector('#display');
@@ -51,10 +18,30 @@ reset.addEventListener('click', () => {
     b = 0;
 });
 
+let backspace = document.querySelector('#backspace');
+backspace.addEventListener('click', () => {
+    displayValue = displayValue.toString();
+    displayValue = displayValue.substring(0, displayValue.length - 1);
+    display.textContent = displayValue;
+    if (displayValue == '') {
+        display.textContent = 0;
+    }
+});
+
+let point = document.querySelector('#point');
+point.addEventListener('click', () => {
+    if (displayValue == '') {displayValue += '0';};
+    if (displayValue.indexOf('.') == -1) {
+        displayValue += '.';
+        display.textContent = displayValue;
+    };
+});
+
 let displayValue = '';
 let nums = Array.from(document.querySelectorAll('.nums'));
 nums.forEach(num => num.addEventListener('click', ()=> {
-    displayValue += parseInt(num.textContent);
+    if (displayValue.length>10) {return};
+    displayValue += parseFloat(num.textContent);
     display.textContent = displayValue;
     })
 );
@@ -65,21 +52,76 @@ let operator = '';
 
 let plus = document.querySelector('#plus');
 plus.addEventListener('click', () => {
+    if (displayValue == '') {return};
     if (operator !== '') {
-        b = parseInt(displayValue);
+        b = parseFloat(displayValue);
         displayValue = operate(operator, a, b);
-        display.textContent = displayValue;
+        display.textContent = parseFloat(displayValue);
     }
-    a = parseInt(displayValue);
+    a = parseFloat(displayValue);
     displayValue = '';
     operator = '+';
 });
 
+
+let minus = document.querySelector('#minus');
+minus.addEventListener('click', () => {
+    if (displayValue == '') {return};
+    if (operator !== '') {
+        b = parseFloat(displayValue);
+        displayValue = operate(operator, a, b);
+        display.textContent = parseFloat(displayValue);
+    }
+    a = parseFloat(displayValue);
+    displayValue = '';
+    operator = '-';
+});
+
+let multiply = document.querySelector('#multiply');
+multiply.addEventListener('click', () => {
+    if (displayValue == '') {return};
+    if (operator !== '') {
+        b = parseFloat(displayValue);
+        displayValue = operate(operator, a, b);
+        display.textContent = parseFloat(displayValue);
+    }
+    a = parseFloat(displayValue);
+    displayValue = '';
+    operator = '*';
+});
+
+let divide = document.querySelector('#divide');
+divide.addEventListener('click', () => {
+    if (displayValue == '') {return};
+    if (operator !== '') {
+        b = parseFloat(displayValue);
+        displayValue = operate(operator, a, b);
+        display.textContent = parseFloat(displayValue);
+    }
+    a = parseFloat(displayValue);
+    displayValue = '';
+    operator = '/';
+});
+
 let equals = document.querySelector('#equals');
 equals.addEventListener('click', () => {
-    b = parseInt(displayValue);
+    if (operator=='') {return};
+    b = parseFloat(displayValue);
     displayValue = operate(operator, a, b);
-    display.textContent = displayValue;
-    a = parseInt(displayValue);
+    display.textContent = parseFloat(displayValue);
+    a = parseFloat(displayValue);
     b = 0;
+    operator = '';
 });
+
+document.addEventListener ('click', () => {
+    if (display.textContent == Infinity || display.textContent == -Infinity) {
+        displayValue = '';
+        operator = '';
+        a = 0;
+        b = 0;
+        display.textContent = 'Rickroll';
+        setTimeout(() => window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), 1000);
+    }
+});
+
